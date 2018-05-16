e33b9be467927c9a639aab9fb669af5fc3e57fb8
Roberto Rubio <00259517@uca.edu.sv>
2018-05-16 16:25:53 -0600

    Creando el generador de elfos, falta agregarle llas clases a orcos y el Reinos para crear las factorys

--- src/Elfos/GeneradorElfos.java
+++ src/Elfos/GeneradorElfos.java
@@ -1,6 +1,23 @@
 package Elfos;
 
+//import Orcos;
+//import Reinos;
 
+public class GeneradorElfos implements Reinos{
+    @Override
+    public Orcos getOrco(String){
+        return null;
+    }
 
-public class GeneradorElfos {
+    @Override
+    public Elfos getElfo(String Type){
+        switch (Type){
+            case "Arquero":
+                return new Arqueros();
+            case "Mago":
+                return new Magos();
+            case "Rey":
+                return new Rey;
+        }
+    }
 }
